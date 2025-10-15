ab0c15e6de4ea836ed611bce98c5703073c17a56
Projjwal Raj <piyushraj707@gmail.com>
2025-10-16 00:26:26 +0530

    Modified SegTree

--- Segment Tree.cpp
+++ Segment Tree.cpp
@@ -14,22 +14,22 @@ public:
 	SegTree(const vector<int>& arr) {
 		N = arr.size();
 		while (__builtin_popcount(N) != 1) N++;
-		tree.resize(2*N);
+		tree.resize(2*N); //may require changes, e.g. initialize with INFINITY in case of min-max problem
 		for (int i=0; i<arr.size(); i++) tree[i+N] = arr[i];
-		for (int i=N-1; i>0; i--) tree[i] = tree[2*i] + tree[2*i+1];
+		for (int i=N-1; i>0; i--) tree[i] = tree[2*i] + tree[2*i+1]; //may require changes
 	}
-	ll f(int ql, int qr, int newVal, int node, int l, int r) {
+	ll f(int ql, int qr, ll newVal, int node, int l, int r) {
 		if (ql > r || qr < l) return 0;
 		if (ql <= l && qr >= r) {
 			if (newVal != -1e18) tree[node] = newVal;
 			return tree[node];
 		}
 		int mid = (l+r)/2;
-		ll sum = f(ql, qr, newVal, 2*node, l, mid) + f(ql, qr, newVal, 2*node+1, mid+1, r);
-		tree[node] = tree[2*node] + tree[2*node+1];
+		ll sum = f(ql, qr, newVal, 2*node, l, mid) + f(ql, qr, newVal, 2*node+1, mid+1, r); //may require changes
+		tree[node] = tree[2*node] + tree[2*node+1]; //may require changes
 		return sum;
 	}
-	ll f(int ql, int qr, int newVal = -1e18) {
+	ll f(int ql, int qr, ll newVal = -1e18) {
 		return f(ql, qr, newVal, 1, 0, N-1);
 	}
 };
